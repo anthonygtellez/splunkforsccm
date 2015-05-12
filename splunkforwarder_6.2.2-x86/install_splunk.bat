0418fec6ec7e0d47d4473c0fdcf7733cd8b052bf
Anthony Tellez <anthonygtellez@gmail.com>
2015-05-12 18:33:21 -0400

    Sleep doesn't solve issue
    Adding a Sleep does not resolve the install issue. The .bat file must
    be executed via cmd prompt or powershell as an administrator, this way
    it goes through line by line installing splunk uf, then adding folders
    and lastly starting the service.

--- splunkforwarder_6.2.2-x86/install_splunk.bat
+++ splunkforwarder_6.2.2-x86/install_splunk.bat
@@ -1,4 +1,3 @@
 msiexec.exe /i "splunkforwarder-6.2.2-255606-x64-release.msi" LAUNCHSPLUNK=0 AGREETOLICENSE=Yes INSTALLDIR="%ProgramFiles%\SplunkUniversalForwarder" SERVICESTARTTYPE=auto /quiet
-SLEEP 10
 xcopy org_all_deploymentclient "%ProgramFiles%\SplunkUniversalForwarder\etc\apps\org_all_deploymentclient" /s /e /i
 NET START SplunkForwarder
