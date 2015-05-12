1474e53145c20e87a140727a2acec5528aa3d973
Anthony Tellez <anthonygtellez@gmail.com>
2015-05-12 19:37:02 -0400

    copy pasta'd the wrong executable in the bat for 6.1.7 & 6.2.2 x86.

--- splunkforwarder_6.1.7-x64/install_splunk.bat
+++ splunkforwarder_6.1.7-x64/install_splunk.bat
@@ -1,3 +1,3 @@
-msiexec.exe /i "splunkforwarder-6.2.2-255606-x64-release.msi" LAUNCHSPLUNK=0 AGREETOLICENSE=Yes INSTALLDIR="%ProgramFiles%\SplunkUniversalForwarder" SERVICESTARTTYPE=auto /quiet
+msiexec.exe /i "splunkforwarder-6.1.7-260715-x64-release.msi" LAUNCHSPLUNK=0 AGREETOLICENSE=Yes INSTALLDIR="%ProgramFiles%\SplunkUniversalForwarder" SERVICESTARTTYPE=auto /quiet
 xcopy org_all_deploymentclient "%ProgramFiles%\SplunkUniversalForwarder\etc\apps\org_all_deploymentclient" /s /e /i
 NET START SplunkForwarder
