1cc53aef950ca41fb1629efba30f3c385f8b322b
Anthony Tellez <anthonygtellez@gmail.com>
2015-05-12 18:02:15 -0400

    Updated SCCM packages
    Windows Server 2003 can only use < Splunk UF 6.1.7, Removed REM's from
    bat script they are no longer needed.

---
+++ splunkforwarder_6.1.7-x86/install_splunk.bat
@@ -0,0 +1,4 @@
+msiexec.exe /i "splunkforwarder-6.1.7-260715-x86-release.msi" LAUNCHSPLUNK=0 AGREETOLICENSE=Yes INSTALLDIR="%ProgramFiles%\SplunkUniversalForwarder" SERVICESTARTTYPE=auto /quiet
+xcopy org_all_deploymentclient "%ProgramFiles%\SplunkUniversalForwarder\etc\apps\org_all_deploymentclient" /s /e /i
+NET START SplunkForwarder
+
